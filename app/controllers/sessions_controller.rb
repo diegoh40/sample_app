f01fe8215434e643eefe0674baef4e34cc792521
Diego <godie8a@hotmail.com>
2024-06-02 08:30:10 -0500

    account activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,14 +6,22 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase) #params viene del form
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] ? remember(user) : forget(user)
-      redirect_back_or user
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        message = "Account not activated. "
+        message += "Check you email for the activation link"
+        flash[:warning]= message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new', status: :unprocessable_entity
     end
   end
+  
 
   def destroy
     log_out if logged_in?
